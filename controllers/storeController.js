dab8930cd5803a4da6af52553497c2073a836f77
Gilbert <gilbertlucas46@gmail.com>
2019-09-08 23:28:07 +0800

    handled edit update of the listings

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -25,3 +25,17 @@ exports.getStore = async (req, res) => {
   console.log(stores)
   res.render('stores', {title: 'Stores', stores})
 }
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({_id: req.params.id});
+  res.render('editStore', {title: `Edit store ${store.name}`, store})
+}
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
+    new: true, //returns a new store instead of the old one
+    runValidators: true,
+  }).exec() //exec() will run the query findOneAndUpdate(_id = query)
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">${store.name}</a></a>`)
+  res.redirect(`/stores/${store._id}/edit`)
+}
